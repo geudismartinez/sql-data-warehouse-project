1b4f65ea903f80c4b3fe05d6edb4039a85ced731
geudismartinez <geudismartinez@outlook.com>
2025-09-21 19:19:31 -0400

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,3 +1,77 @@
+/*
+============================================================
+Script DDL: Crear tablas Bronze
+============================================================
+
+Propósito del script:
+Este script crea tablas en el esquema 'bronze', eliminando las tablas existentes
+si ya existen.
+Ejecuta este script para redefinir la estructura DDL de las tablas 'bronze'.
+============================================================
+*/
+
+-- Eliminar tabla si existe
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+GO
+
+-- Tabla de clientes
+CREATE TABLE bronze.crm_cust_info (
+    cst_id INT,
+    cst_key NVARCHAR(50),
+    cst_firstname NVARCHAR(50),
+    cst_lastname NVARCHAR(50),
+    cst_marital_status NVARCHAR(50),
+    cst_gndr NVARCHAR(50),
+    cst_create_date DATE
+);
+
+-- Tabla de productos
+CREATE TABLE bronze.crm_prd_info (
+    prd_id INT,
+    prd_key NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost INT,
+    prd_line NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt DATETIME
+);
+
+-- Detalles de ventas
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num NVARCHAR(50),
+    sls_prd_key NVARCHAR(50),
+    sls_cust_id INT,
+    sls_order_dt INT,
+    sls_ship_dt INT,
+    sls_due_dt INT,
+    sls_sales INT,
+    sls_quantity INT,
+    sls_price INT
+);
+
+-- Ubicación ERP
+CREATE TABLE bronze.erp_loc_a101 (
+    cid NVARCHAR(50),
+    CNTRY NVARCHAR(50)
+);
+
+-- Clientes ERP
+CREATE TABLE bronze.erp_cust_az12 (
+    cid NVARCHAR(50),
+    bdate DATE,
+    gen NVARCHAR(50)
+);
+
+-- Categorías de precios ERP
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id NVARCHAR(50),
+    cat NVARCHAR(50),
+    subcat NVARCHAR(50),
+    maintenance NVARCHAR(50)
+);
+Creado 21-09-2025
+Por Geudis Martinez
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS 
 BEGIN
 	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME , @batch_end_time DATETIME;
